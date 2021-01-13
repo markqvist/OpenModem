7f8dfae211a5456ed92a64e7470bb76f5008c15d
Dmitriy Logachov <demonwork@yandex.ru>
2021-01-13 15:10:59 +0500

    Added the ability to invert SD_DETECT for cardholders with a normally open contact to determine the presence of a card.

--- protocol/KISS.h
+++ protocol/KISS.h
@@ -37,6 +37,7 @@
 #define CMD_NMEA 0x40
 #define CMD_PRINT_CONFIG 0xF0
 #define CMD_RETURN 0xFF
+#define CMD_INVERT_SDDETECT 0x15
 
 void kiss_init(AX25Ctx *ax25, Afsk *afsk, Serial *ser);
 void kiss_messageCallback(AX25Ctx *ctx);
